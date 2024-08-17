a8b3908685a50f0a2ca417c328abf677734cf679
Vinothhk <iamvinothkumar16@gmail.com>
2024-08-17 21:06:49 +0530

    added arm image

--- miniproject/button.py
+++ miniproject/button.py
@@ -8,13 +8,13 @@ y_pos = 0
 def update_x(delta):
     global x_pos
     x_pos += delta
-    x_value.set(f'X: {x_pos}')
+    x_value.set(f'X: {x_pos} deg')
     # Add code to send the updated x_pos value to the servo
 
 def update_y(delta):
     global y_pos
     y_pos += delta
-    y_value.set(f'Y: {y_pos}')
+    y_value.set(f'Y: {y_pos} deg')
     # Add code to send the updated y_pos value to the servo
 
 # Set x and y to home position
@@ -22,14 +22,23 @@ def move_to_home():
     global x_pos, y_pos
     x_pos = 45  # Home x value
     y_pos = 45 # Home y value
-    x_value.set(f'X: {x_pos}')
-    y_value.set(f'Y: {y_pos}')
+    x_value.set(f'X: {x_pos} deg')
+    y_value.set(f'Y: {y_pos} deg')
     # Add code to send the home position to the servos
 
 # Create the main window
 window = tk.Tk()
 window.title("2R Robot Control")
-window.geometry('300x300')
+window.geometry('600x600')
+
+arm_img = Image.open("./miniproject/manipulator.jpg")
+arm_img = arm_img.resize((300,300))
+arm_img = ImageTk.PhotoImage(arm_img)  # Replace with your image file
+
+# Create a label to hold the image
+image_label = tk.Label(window, image=arm_img)
+image_label.pack()
+
 image = Image.open("./miniproject/home.png") # Load your jpg image
 image = image.resize((20, 20))
 home_icon = ImageTk.PhotoImage(image)
@@ -37,8 +46,8 @@ home_icon = ImageTk.PhotoImage(image)
 # Labels to display current positions
 x_value = tk.StringVar()
 y_value = tk.StringVar()
-x_value.set(f"X: {x_pos}")
-y_value.set(f"Y: {y_pos}")
+x_value.set(f"X: {x_pos} deg")
+y_value.set(f"Y: {y_pos} deg")
 
 label_x = tk.Label(window, textvariable=x_value)
 label_x.pack()
@@ -50,21 +59,21 @@ label_y.pack()
 frame_x = tk.Frame(window)
 frame_x.pack()
 
-btn_x_minus = tk.Button(frame_x, text="X-", command=lambda: update_x(-1))
-btn_x_minus.pack(side='left', padx=10)
+btn_x_minus = tk.Button(frame_x, text="X-", command=lambda: update_x(-1),width=16)
+btn_x_minus.pack(side='left', padx=20,pady=10)
 
-btn_x_plus = tk.Button(frame_x, text="X+", command=lambda: update_x(1))
-btn_x_plus.pack(side='right', padx=10)
+btn_x_plus = tk.Button(frame_x, text="X+", command=lambda: update_x(1),width=16)
+btn_x_plus.pack(side='right', padx=20,pady=10)
 
 # Buttons for y-axis control
 frame_y = tk.Frame(window)
 frame_y.pack()
 
-btn_y_minus = tk.Button(frame_y, text="Y-", command=lambda: update_y(-1))
-btn_y_minus.pack(side='left', padx=10)
+btn_y_minus = tk.Button(frame_y, text="Y-", command=lambda: update_y(-1),width=16)
+btn_y_minus.pack(side='left', padx=20,pady=10)
 
-btn_y_plus = tk.Button(frame_y, text="Y+", command=lambda: update_y(1))
-btn_y_plus.pack(side='right', padx=10)
+btn_y_plus = tk.Button(frame_y, text="Y+", command=lambda: update_y(1),width=16)
+btn_y_plus.pack(side='right', padx=20,pady=10)
 
 # Home button to reset to the home position
 btn_home = tk.Button(window, text="Home",image=home_icon,compound=tk.LEFT, command=move_to_home)
